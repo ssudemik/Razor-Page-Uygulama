02495c0c7f1b005c67ee66d43bd0978cf762ce46
ssudemik <sude.emik@stu.fbu.edu.tr>
2024-11-24 01:02:20 +0300

    Razor Page Uygulama
    -

---
+++ RazorPageDeneme/RazorPageDeneme/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPageDeneme.Models
+{
+    public class Context : DbContext //database set gibi komutları kullanabilmek için miras alma işlemi
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyDb");
+        }
+
+    }
+}
